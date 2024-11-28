1c80a90d275efd11acbfc3415c81488db164b6ae
countingbeeps <gamedungeon@protonmail.com>
2024-11-27 20:50:52 -0600

    fix chmod pls

--- mobsf/MobSF/init.py
+++ mobsf/MobSF/init.py
@@ -4,6 +4,7 @@ import os
 import random
 import subprocess
 import sys
+from stat import S_IWUSR, S_IREAD
 import shutil
 import threading
 from hashlib import sha256
@@ -154,9 +155,9 @@ def get_mobsf_home(use_home, base_dir):
                 shutil.copytree(src, sig_dir, dirs_exist_ok=True)
                 for root, dirs, files in os.walk(sig_dir):
                     for dir in dirs:
-                        os.chmod(os.path.join(root, dir), 0o755)
+                        os.chmod(os.path.join(root, dir), S_IWUSR|S_IREAD)
                     for file in files:
-                        os.chmod(os.path.join(root, file), 0o755)
+                        os.chmod(os.path.join(root, file), S_IWUSR|S_IREAD)
             except Exception:
                 pass
         return mobsf_home.as_posix()
